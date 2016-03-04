cb470b8cf7ab90f2edce9c3261bf37d742bc3c53
michaelmartinez <martinezmj@uthscsa.edu>
2016-03-04 15:01:18 -0600

    added new references to surface files

--- src/edu/uthscsa/ric/papaya/builder/Builder.java
+++ src/edu/uthscsa/ric/papaya/builder/Builder.java
@@ -76,16 +76,16 @@ public class Builder {
 	public static final String JS_FILE_JQUERY = "lib/jquery.js";
 	public static final String JS_FILE_DAIKON = "lib/daikon.js";
 	public static final String[] JS_FILES = { "lib/base64-binary.js", "lib/bowser.js", "lib/numerics.js", "lib/pako-inflate.js", "lib/nifti-reader.js",
-			"src/js/constants.js", "src/js/utilities/array-utils.js", "src/js/utilities/math-utils.js", "src/js/utilities/object-utils.js",
-			"src/js/utilities/platform-utils.js", "src/js/utilities/string-utils.js", "src/js/utilities/url-utils.js", "src/js/core/coordinate.js",
-			"src/js/core/point.js", "src/js/volume/header.js", "src/js/volume/imagedata.js", "src/js/volume/imagedescription.js",
+			"lib/gifti-reader.js", "lib/gl-matrix.js", "src/js/constants.js", "src/js/utilities/array-utils.js", "src/js/utilities/math-utils.js",
+			"src/js/utilities/object-utils.js", "src/js/utilities/platform-utils.js", "src/js/utilities/string-utils.js", "src/js/utilities/url-utils.js",
+			"src/js/core/coordinate.js", "src/js/core/point.js", "src/js/volume/header.js", "src/js/volume/imagedata.js", "src/js/volume/imagedescription.js",
 			"src/js/volume/imagedimensions.js", "src/js/volume/imagerange.js", "src/js/volume/imagetype.js", "src/js/volume/nifti/header-nifti.js",
 			"src/js/volume/dicom/header-dicom.js", "src/js/volume/orientation.js", "src/js/volume/transform.js", "src/js/volume/volume.js",
-			"src/js/volume/voxeldimensions.js", "src/js/volume/voxelvalue.js", "src/js/ui/dialog.js", "src/js/ui/menu.js", "src/js/ui/menuitem.js",
-			"src/js/ui/menuitemcheckbox.js", "src/js/ui/menuitemradiobutton.js", "src/js/ui/menuitemfilechooser.js", "src/js/ui/menuitemrange.js",
-			"src/js/ui/menuitemslider.js", "src/js/ui/menuitemspacer.js", "src/js/ui/toolbar.js", "src/js/viewer/atlas.js", "src/js/viewer/colortable.js",
-			"src/js/viewer/display.js", "src/js/viewer/preferences.js", "src/js/viewer/screenslice.js", "src/js/viewer/screenvol.js",
-			"src/js/viewer/viewer.js", "src/js/main.js", "src/js/license.js" };
+			"src/js/volume/voxeldimensions.js", "src/js/volume/voxelvalue.js", "src/js/surface/surface.js", "src/js/ui/dialog.js", "src/js/ui/menu.js",
+			"src/js/ui/menuitem.js", "src/js/ui/menuitemcheckbox.js", "src/js/ui/menuitemradiobutton.js", "src/js/ui/menuitemfilechooser.js",
+			"src/js/ui/menuitemrange.js", "src/js/ui/menuitemslider.js", "src/js/ui/menuitemspacer.js", "src/js/ui/toolbar.js", "src/js/viewer/atlas.js",
+			"src/js/viewer/colortable.js", "src/js/viewer/display.js", "src/js/viewer/preferences.js", "src/js/viewer/screenslice.js",
+			"src/js/viewer/screenvol.js", "src/js/viewer/viewer.js", "src/js/viewer/screensurface.js", "src/js/main.js", "src/js/license.js" };
 	public static final String[] CSS_FILES = { "src/css/base.css", "src/css/ui/toolbar.css", "src/css/ui/menu.css", "src/css/ui/dialog.css",
 			"src/css/utilities/nojs.css", "src/css/utilities/unsupported.css", "src/css/viewer/viewer.css" };
 	public static final String RESOURCE_HTML = "index.html";
